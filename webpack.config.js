64a7d4a321651586bcbf82740ec606a74b0b23f8
fireliangbin <451061218@qq.com>
2016-03-01 16:02:10 +0800

    t
    t

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,17 @@ module.exports = {
         publicPath: '/static/'
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'DEBUG': true
             }
-        }),
-        new webpack.HotModuleReplacementPlugin()
+        })
     ],
     module: {
+        preLoaders: [
+            {test: [/\.js$/, /\.jsx$/], loader: "eslint-loader", exclude: /node_modules/}
+        ],
         loaders: [
             {test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/},
             {test: /\.js?$/, loaders: ['react-hot', 'babel'], include: [path.join(__dirname, 'src')]}
