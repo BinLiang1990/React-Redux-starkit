7c3d409b164ef7250ae8ad5f2afb7d87f9b2716b
fireliangbin <451061218@qq.com>
2016-03-01 15:46:43 +0800

    t
    t

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/**
+ * Created by EdenLiang on 2016/1/20.
+ */
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello, world1!</h1>,
+    document.getElementById('content')
+);
