9982660332c6bc616577f9a00d882c3b6d0d1f23
edenliang <451061218@qq.com>
2016-03-08 15:04:51 +0800

    Eden.Liang

---
+++ src/js/Containers/root.dev.js
@@ -0,0 +1,45 @@
+/**
+ * Created by EdenLiang on 2016/3/8.
+ */
+'use strict';
+import React, { PropTypes } from 'react';
+import { Router, Route,browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import ConfigureStore from './../store/configureStore';
+import DevTools from './devTools';
+import Home from './home';
+import BaseInfo from './baseInfo';
+import Menu from './menu';
+import LeftBar from './leftbar';
+
+const store = ConfigureStore();
+
+const App = (props) => {
+    return (
+        <div>
+            <LeftBar />
+            {props.children}
+        </div>
+    );
+}
+
+
+const Root = () => {
+    return (
+        <div>
+            <Provider store={store}>
+                <Router history={browserHistory}>
+                    <Route component={App}>
+                        <Route path="/" component={Home}>1</Route>
+                        <Route path="baseinfo" component={BaseInfo}>1</Route>
+                        <Route path="menu" component={Menu}>2</Route>
+                    </Route>
+                </Router>
+            </Provider>
+            <DevTools store={store} />
+        </div>
+    );
+};
+
+
+export  default Root
