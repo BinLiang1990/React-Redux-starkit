a1f8a779974eb8930d501d60a8f7de39b5c903e4
edenliang <451061218@qq.com>
2016-03-15 11:25:37 +0800

    2016-3-15 11:25:34

--- src/js/index.js
+++ src/js/index.js
@@ -7,14 +7,5 @@ import 'fetch-ie8';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './containers/root';
-//import { createStore } from 'redux'
-//import { Provider } from 'react-redux';
-//import index from './js/reducers/index';
-//import App from './js/containers/app';
 
-
-//const store = createStore(index);
-//ReactDOM.render(<Provider store={store}>
-//    <App />
-//</Provider>, document.getElementById('content'));
 ReactDOM.render(<Root />, document.getElementById('content'));
